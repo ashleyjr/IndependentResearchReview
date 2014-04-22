143479f8befc1175b8cf46b2185cba0698b365bd
Ashley Robinson <ajr2g10@ecs.soton.ac.uk>
2014-04-22 16:46:42 +0100

    final

---
+++ Final/make.bat
@@ -0,0 +1,13 @@
+pdflatex AshleyRobinson.tex
+bibtex AshleyRobinson
+pdflatex AshleyRobinson.tex 
+pdflatex AshleyRobinson.tex 
+texcount -v -html -inc AshleyRobinson.tex > count.html
+del .pdf
+del *.aux
+del *.bbl
+del *.blg
+del *.log
+del *.synctex.gz
+del *.toc
+
